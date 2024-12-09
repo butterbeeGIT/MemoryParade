01386bd1814c058ad2f0d75072fe49a3e9bb3735
Almazova Vlada <almazova.2004@mail.ru>
2024-12-09 21:29:29 +0500

    переход в сцену меню

---
+++ Assets/MemoryParade/Scripts/Game/forSceneTransition/GoToMainMenuButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Assets.MemoryParade.Scripts.Game.GameRoot;
+
+namespace Assets.MemoryParade.Scripts.Game.forSceneTransition
+{
+    public class GoToMainMenuButton : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            Debug.Log("Нажата кнопка меню");
+            SceneTransitionManager.Instance.GoToMenu();
+        }
+    }
+}
