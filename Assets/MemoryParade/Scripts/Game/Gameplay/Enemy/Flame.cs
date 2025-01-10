943d37a036db041ac2b70d4794d4ed0768fedb8d
Almazova Vlada <almazova.2004@mail.ru>
2025-01-11 00:40:10 +0500

    свойства врагов

---
+++ Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Flame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.MemoryParade.Scripts.Game.Gameplay.Enemy
+{
+    /// <summary>
+    /// Класс слайма
+    /// </summary>
+    public class Flame : Enemy
+    {
+        private void Awake()
+        {
+            EnemyType = "Flame";
+            MaxHealth = 30;
+            Health = MaxHealth;
+            Attack = 5;
+            Speed = 3f;
+            RewardPoints = 3;
+        }
+
+        public override void AttackPlayer(GameObject player)
+        {
+            Debug.Log($"{EnemyType} attacks player for {Attack} damage.");
+            // TODO Логика атаки
+        }
+    }
+}
---
+++ Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Slime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.MemoryParade.Scripts.Game.Gameplay.Enemy
+{
+    /// <summary>
+    /// Класс слайма
+    /// </summary>
+    public class Slime : Enemy
+    {
+        private void Awake()
+        {
+            EnemyType = "Slime";
+            MaxHealth = 20;
+            Health = MaxHealth;
+            Attack = 2;
+            Speed = 2f;
+            RewardPoints = 1;
+        }
+
+        public override void AttackPlayer(GameObject player)
+        {
+            Debug.Log($"{EnemyType} attacks player for {Attack} damage.");
+            // TODO Логика атаки
+        }
+    }
+}
