905118c30266adb56238543384b5a3cf5a10a12b
Almazova Vlada <almazova.2004@mail.ru>
2025-01-13 22:15:47 +0500

    враги не проходят сквозь стены

--- Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Follow.cs
+++ Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Follow.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-
+/// <summary>
+/// Движение врага по направлению к игроку
+/// </summary>
 public class Follow : MonoBehaviour
 {
     public float speed;
     public Transform player;
     public SpriteRenderer sr;
     private Animator animator;
+
+    // Длина луча для проверки препятствий
+    public float raycastDistance = 0.5f;
+    // Слой, на котором находятся стены
+    public LayerMask wallLayer;
 
     void Awake()
     {
@@ -16,15 +23,28 @@ public class Follow : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
-        var direction = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+        // Направление движения врага к игроку
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, raycastDistance, wallLayer);
+
+        if (hit.collider == null) // Если препятствий нет
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+        }
+
+        //transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+        //var direction = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+
         if (direction.x > 0)
             sr.flipX = true;
         else sr.flipX = false;
-        if (Vector2.Distance(player.transform.position, animator.transform.position) < 0.1f)
+
+        //if (Vector2.Distance(player.transform.position, animator.transform.position) < 0.1f)
+        if (Vector2.Distance(player.position, transform.position) < 0.1f)
         {
             animator.SetBool("stay", true);
             sr.flipX = true;
-        }
+        }  
     }
 }
