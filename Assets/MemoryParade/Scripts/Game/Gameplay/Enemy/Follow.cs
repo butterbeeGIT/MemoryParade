2fe1a8b5db4c56f016b878c064afe8587edcec8a
Almazova Vlada <almazova.2004@mail.ru>
2025-01-14 23:21:31 +0500

    изменение области видимости

--- Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Follow.cs
+++ Assets/MemoryParade/Scripts/Game/Gameplay/Enemy/Follow.cs
@@ -9,6 +9,8 @@ public class Follow : MonoBehaviour
     private SpriteRenderer sr;
     private Animator animator;
     public bool canBattle = false;
+    //зона видимости
+    private float visibilityArea;
 
     // Длина луча для проверки препятствий
     public float raycastDistance = 0.5f;
@@ -17,6 +19,8 @@ public class Follow : MonoBehaviour
 
     void Awake()
     {
+        speed = 0.01f;
+        visibilityArea = 2f;
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         sr = GetComponent<SpriteRenderer>();
@@ -26,29 +30,34 @@ public class Follow : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Направление движения врага к игроку
-        Vector2 direction = (player.position - transform.position).normalized;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, raycastDistance, wallLayer);
-
-        if (hit.collider == null) // Если препятствий нет
+        if (Vector2.Distance(player.position, transform.position) < visibilityArea)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+            Debug.Log("Персонаж обнаружен!");
+            // Направление движения врага к игроку
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, raycastDistance, wallLayer);
+
+            if (hit.collider == null) // Если препятствий нет
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+            }
+
+            if (direction.x > 0)
+                sr.flipX = true;
+            else sr.flipX = false;
+
+            if (Vector2.Distance(player.position, transform.position) < 0.1f)
+            {
+                animator.SetBool("stay", true);
+                canBattle = true;
+                sr.flipX = true;
+            }
         }
-
-        //transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
-        //var direction = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
-
-        if (direction.x > 0)
-            sr.flipX = true;
-        else sr.flipX = false;
-
-        //if (Vector2.Distance(player.transform.position, animator.transform.position) < 0.1f)
-        if (Vector2.Distance(player.position, transform.position) < 0.1f)
+        else
         {
-            animator.SetBool("stay", true);
-            canBattle = true;
-            sr.flipX = true;
-        }  
+            Debug.Log("Персонаж покинул область!");
+            //speed = 0f;
+        }    
     }
 }
